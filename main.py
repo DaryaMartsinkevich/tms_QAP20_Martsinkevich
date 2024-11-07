33ddee487175b435852f11b14b2133313f7cc122
DaryaMartsinkevich <daryamartsinkevich@gmail.com>
2024-11-07 19:49:33 +0100

    add main

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello!")
+err = {
+    'length': 'Длина пароля не равна 8 символам',
+    'upper': 'Отсутствуют заглавные буквы',
+    'lower': 'Нет строчных букв в пароле',
+    'digits': 'Нет цифр в пароле',
+    }
+
+password = input('Пароль:')
+if len(password) < 10:
+    print(err['length'])
+if password.islower() == True:
+    print(err['upper'])
+if password.isupper() == True:
+    print(err['lower'])
+if password.isdigit() == False:
+    print(err['digits'])
+else:
+    print('PASS')
